8842d671e1331a2dabdf0016ac3a91cd76a09d1c
LayanaRiboValide <96994493+LayanaRiboValide@users.noreply.github.com>
2022-01-04 10:30:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const scientist = document.getElementById('scientist');
+const gas = document.getElementById('gas');
+
+window.addEventListener('scroll', function(){
+
+  scrollValue = (window.innerHeight + window.scrollY) / (document.body.offsetHeight);
+
+  if (scrollValue > 0.62){
+    scientist.style.opacity = '1';
+    scientist.style.transform = 'none';
+  };
+
+  if (scrollValue > 0.60){
+    gas.style.opacity = '1';
+    gas.style.transform = 'none';
+  };
+});
